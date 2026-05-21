f709f119284bd83696507d9c98d443dd4a69c140
jahniasfnnn-ux <jahniasfnnn@gmail.com>
2026-05-21 14:08:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-import os, telebot
+import telebot
 from flask import Flask
 from threading import Thread
 
-# إعداد البوت
-bot = telebot.TeleBot(os.environ.get('TOKEN'))
+# ضع التوكين هنا مباشرة
+TOKEN = "8584559549:AAE5cOA8MvZu5yTvrop2QI0WlYqB_bVz-88" 
 
-# خادم ويب وهمي (ضروري جداً في Render)
+bot = telebot.TeleBot(TOKEN)
+
 app = Flask(__name__)
 @app.route('/')
-def home(): return "Bot is running!"
+def home(): return "Bot is working!"
 def run(): app.run(host='0.0.0.0', port=10000)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "البوت يعمل الآن بنجاح!")
+    bot.send_message(message.chat.id, "تم التشغيل بنجاح وبدون إعدادات!")
 
 if __name__ == "__main__":
     Thread(target=run).start()
